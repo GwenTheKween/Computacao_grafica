b769375fdca29135629f8d8bf8881e00c1740534
billionai <larsen.bruno.p@gmail.com>
2019-03-27 11:44:20 -0300

    started the setup for dm

--- dm.cpp
+++ dm.cpp
@@ -1,1 +1,112 @@
 #include "dm.h"
+
+//===================================================================
+//Auxiliary funtions and structures
+
+bool isPhysicalDeviceSuitable(VkPhysicalDevice dev){
+	//better suitability check TBA
+	return true;
+}
+
+//===================================================================
+//Private Methods implementations
+
+void DisplayManager::initWindow(){
+	//initiates glfw with correct parameters
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(width, height, "projeto 1", nullptr, nullptr);
+}
+
+void DisplayManager::initVulkan(){
+	createInstance();
+
+	//debug layers, TBA
+
+	createSurface();
+
+	pickPhysicalDevice();
+
+//	createLogicalDevice();
+}
+
+void DisplayManager::createInstance(){
+	//information required to start Vulkan instance
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "projeto 1";
+	appInfo.applicationVersion = VK_MAKE_VERSION(0,1,0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+	//struct that will carry the info
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	//extension setup
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	createInfo.enabledLayerCount = 0;
+
+	if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		throw std::runtime_error("Failed to create Vulkan Instance");
+}
+
+void DisplayManager::createSurface(){
+	if(glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS)
+		throw std::runtime_error("failed to create GLFW surface");
+}
+
+void DisplayManager::pickPhysicalDevice(){
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+	if(deviceCount == 0){
+		throw std::runtime_error("No GPUs with Vulkan access available, Have you installed Vulkan's Firmware?");
+	}
+
+	//gets a list of GPUs available, and chooses the best suited
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+	for(int i=0; i<deviceCount; i++){
+		if(isPhysicalDeviceSuitable(devices[i])){
+			physDevice = devices[i];
+			break;
+		}
+	}
+
+	if(physDevice == VK_NULL_HANDLE)
+		throw std::runtime_error("None of the Available GPUs support the desired operations");
+}
+
+//===================================================================
+//Public Methods implementations
+
+DisplayManager::DisplayManager(int wid, int hei):
+	width(wid),
+	height(hei),
+	device(VK_NULL_HANDLE)
+	{
+
+	initWindow();
+	initVulkan();
+
+	}
+
+DisplayManager::~DisplayManager(){
+	vkDestroyDevice(device, nullptr);
+	vkDestroySurfaceKHR(instance, surface, nullptr);
+	vkDestroyInstance(instance, nullptr);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
--- dm.h
+++ dm.h
@@ -1,7 +1,42 @@
 #ifndef DISPLAY_MANAGER
 #define DISPLAY_MANAGER
 
+//standard libraries
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <optional>
+#include <vector>
+#include <set>
+
+//graphic libraries
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 class DisplayManager{
+private:
+	//members used for GLFW library
+	GLFWwindow* window;
+
+	//members used for Vulkan library
+	VkInstance instance;
+	VkSurfaceKHR surface;
+	VkPhysicalDevice physDevice;
+	VkDevice device;
+
+	//General Purpose Members
+	const int width, height;
+
+	//private methods
+	void initWindow();
+	void initVulkan();
+	void createInstance();
+	void createSurface();
+	void pickPhysicalDevice();
+
+public:
+	DisplayManager(int = 800, int = 600);
+	~DisplayManager();
 };
 
 #endif
