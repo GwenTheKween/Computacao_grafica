c7b8f3c21f685ecd5de980f9b4d9d4fbf5a5d766
billionai <larsen.bruno.p@gmail.com>
2019-04-04 14:24:58 -0300

    changed coordinate set for buttons, now compatible with mouse

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ DisplayManager dm;
 std::vector<botao> b;
 
 void nada(){
-	printf("foi\n");
 	return;
 }
 
@@ -15,7 +14,6 @@ void mouseButton(GLFWwindow* w, int button, int action, int mods){
 	if(GLFW_MOUSE_BUTTON_LEFT == button && action == GLFW_PRESS){
 		double x,y;
 		glfwGetCursorPos(w, &x, &y);
-		printf("%lf\t%lf\n",x,y);
 		for(int i=0;i<b.size();i++){
 			if(b[i].inside(x,y)){
 				b[i].press();
@@ -27,15 +25,15 @@ void mouseButton(GLFWwindow* w, int button, int action, int mods){
 
 botao cria_botao_sair(){
 	float vertices[8];
-	vertices[0] = -750;
-	vertices[2] = -500;
-	vertices[4] = -500;
-	vertices[6] = -750;
+	vertices[0] = 50;
+	vertices[2] = 200;
+	vertices[4] = 200;
+	vertices[6] = 50;
 
-	vertices[1] = -550;
-	vertices[3] = -550;
-	vertices[5] = -450;
-	vertices[7] = -450;
+	vertices[1] = 575;
+	vertices[3] = 575;
+	vertices[5] = 525;
+	vertices[7] = 525;
 
 	botao b(vertices,nada);
 	return b;
@@ -44,14 +42,14 @@ botao cria_botao_sair(){
 botao cria_botao_2(){
 	float vertices[8];
 	vertices[0] = 750;
-	vertices[2] = 500;
-	vertices[4] = 500;
+	vertices[2] = 600;
+	vertices[4] = 600;
 	vertices[6] = 750;
 
-	vertices[1] = -550;
-	vertices[3] = -550;
-	vertices[5] = -450;
-	vertices[7] = -450;
+	vertices[1] = 575;
+	vertices[3] = 575;
+	vertices[5] = 525;
+	vertices[7] = 525;
 
 	botao b(vertices,nada);
 	return b;
--- shaders/shader.vert
+++ shaders/shader.vert
@@ -1,8 +1,10 @@
 #version 450 core
 
+//Vertex Shader. Takes coordinates from (0,0) to windowSize, and returns between (-1,1) to (1,-1)
+
 layout (location = 0) in vec2 inPosition;
 uniform ivec2 windowSize;
 
 void main(){
-	gl_Position = vec4(inPosition.x/windowSize.x,inPosition.y/windowSize.y,0.0 ,1.0);
+	gl_Position = vec4((inPosition.x - windowSize.x/2)/(windowSize.x/2),(windowSize.y/2 - inPosition.y)/(windowSize.y/2),0.0 ,1.0);
 }
