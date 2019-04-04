3c887044515114dff583cc0bb164f65a0e87a7d5
billionai <larsen.bruno.p@gmail.com>
2019-04-04 12:01:19 -0300

    criei construtor

--- botoes.h
+++ botoes.h
@@ -1,8 +1,19 @@
+#ifndef BOTOES
+#define BOTOES
 #include "dm.h"
 
 class botao{
 private:
-	unsigned int VAO;
+	VAO_INFO ID;
+	float position[8];
+	void (*action)(void);
 public:
 	botao(){};
-}
+	botao(float*, void (*func)(void));
+
+	void press(){
+		action();
+	}
+};
+
+#endif
